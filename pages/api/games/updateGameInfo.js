0b829ad54359512e9ee6908e637b795df176772e
ToMattBan <souaquelecleyton@gmail.com>
2022-02-28 15:01:47 -0300

    update How get game infos to get portuguese infos

--- pages/api/games/updateGameInfo.js
+++ pages/api/games/updateGameInfo.js
@@ -1,40 +1,63 @@
+const https = require('https');
 import clientPromise from '../../../lib/mongodb'
 
 export default async (req, res) => {
-  const games = await fetch(process.env.NEXT_PUBLIC_URL + '/api/games/getAllGames').then(res => res.json());
-
-  const client = await clientPromise;
-  const db = client.db('games');
+  const games = await fetch(process.env.URL + '/api/games/getAllGames').then(res => res.json());
 
   try {
     games.forEach(async game => {
-      var gameDetails = await fetch(`https://store.steampowered.com/api/appdetails?cc=br&l=br&appids=${game.steamID}`).then(res => res.json());
-      gameDetails = gameDetails[game.steamID].data
-
-      if (gameDetails.movies)
-        var videos = gameDetails.movies.map(video => { return video.webm["480"] })
-
-      var screenshots = gameDetails.screenshots.map(screenshot => { return screenshot.path_thumbnail })
-      
-      await db.collection('games').update(
-        { steamID: game.steamID },
-        { $set: {
-          name: gameDetails.name,
-          complete_description: gameDetails.detailed_description,
-          short_description: gameDetails.short_description,
-          "prices.original": gameDetails.price_overview ? gameDetails.price_overview.initial : 0,
-          "prices.promo": gameDetails.price_overview ? gameDetails.price_overview.final : null,
-          "prices.discount": gameDetails.price_overview ? gameDetails.price_overview.discount_percent : 0,
-          "media.banner": gameDetails.header_image,
-          "media.background": gameDetails.background,
-          "media.videos": videos || [],
-          "media.screenshot": screenshots,
-        } }
-      )
+      getGameData(game.steamID)
     })
     res.json({ worked: true })
   } catch (e) {
     res.send(e)
   }
+}
 
+function getGameData(steamID) {
+  var gameDetails = '';
+
+  https.get({
+    hostname: 'store.steampowered.com',
+    path: `/api/appdetails?cc=br&appids=${steamID}`,
+    method: 'GET',
+    headers: {
+      'Accept-Language': 'pt-BR',
+      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:97.0) Gecko/20100101 Firefox/97.0'
+    }
+  }, async (res) => {
+    res.setEncoding('utf8');
+    res.on('data', data => { gameDetails += data });
+    res.on('end', () => { updateGameData(gameDetails, steamID) })
+  })
+}
+
+async function updateGameData(gameData, steamID) {
+  var gameDetails = (JSON.parse(gameData))[steamID].data
+
+  if (gameDetails.movies)
+    var videos = gameDetails.movies.map(video => { return video.webm["480"] })
+
+  var screenshots = gameDetails.screenshots.map(screenshot => { return screenshot.path_thumbnail })
+
+  const client = await clientPromise;
+  const db = client.db('games');
+
+  await db.collection('games').updateOne(
+    { steamID: steamID },
+    {
+      $set: {
+        name: gameDetails.name,
+        complete_description: gameDetails.detailed_description,
+        short_description: gameDetails.short_description,
+        "prices.original": gameDetails.price_overview ? gameDetails.price_overview.initial : 0,
+        "prices.promo": gameDetails.price_overview ? gameDetails.price_overview.final : null,
+        "prices.discount": gameDetails.price_overview ? gameDetails.price_overview.discount_percent : 0,
+        "media.banner": gameDetails.header_image,
+        "media.background": gameDetails.background,
+        "media.videos": videos || [],
+        "media.screenshot": screenshots,
+      }
+    }
+  )
 }
